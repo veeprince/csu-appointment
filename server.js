4215ad798f606fe2eefaed50a70e43d7c227cc9b
Veeprince <bakinwande656@gmail.com>
2022-01-07 23:44:28 -0600

    Semi functioning UI

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/students');
 const studentRouter = require('./routes/students');
 
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
-app.use(express.static('public'));
+app.use(express.static(`${__dirname}/public`));
 app.use(express.urlencoded({ extended: true }));
+// app.use('/', express.static('assets'));
+// app.use('/assets', express.static('assets'));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
